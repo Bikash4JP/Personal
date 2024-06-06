37ee282fd1d4e1bca2aab4c12a37aed64841c336
bikash4jp <bikash4jp@gmail.com>
2024-06-06 13:02:59 +0900

    messege self clear has bee nadded

--- assets/js/bikash.js
+++ assets/js/bikash.js
@@ -1,16 +1,3 @@
-/*!
-=========================================================
-* JohnDoe Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 // smooth scroll
 $(document).ready(function(){
@@ -32,119 +19,31 @@ $(document).ready(function(){
 }); 
 
 // protfolio filters
-$(window).on("load", function() {
-    var t = $(".portfolio-container");
-    t.isotope({
-        filter: ".new",
-        animationOptions: {
-            duration: 750,
-            easing: "linear",
-            queue: !1
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var form = this;
+    var data = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
         }
-    }), $(".filters a").click(function() {
-        $(".filters .active").removeClass("active"), $(this).addClass("active");
-        var i = $(this).attr("data-filter");
-        return t.isotope({
-            filter: i,
-            animationOptions: {
-                duration: 750,
-                easing: "linear",
-                queue: !1
-            }
-        }), !1
+    }).then(response => {
+        if (response.ok) {
+            alert('Your message has been sent successfully!');
+            form.reset();
+        } else {
+            return response.json().then(data => {
+                if (Object.hasOwnProperty.call(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert('Oops! There was a problem submitting your form');
+                }
+            })
+        }
+    }).catch(error => {
+        alert('Oops! There was a problem submitting your form');
     });
 });
-
-
-// // google maps
-// function initMap() {
-// // Styles a map in night mode.
-//     var map = new google.maps.Map(document.getElementById('map'), {
-//         center: {lat: 40.674, lng: -73.945},
-//         zoom: 12,
-//         scrollwheel:  false,
-//         navigationControl: false,
-//         mapTypeControl: false,
-//         scaleControl: false,
-//       styles: [
-//         {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-//         {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-//         {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-//         {
-//           featureType: 'administrative.locality',
-//           elementType: 'labels.text.fill',
-//           stylers: [{color: '#d59563'}]
-//         },
-//         {
-//           featureType: 'poi',
-//           elementType: 'labels.text.fill',
-//           stylers: [{color: '#d59563'}]
-//         },
-//         {
-//           featureType: 'poi.park',
-//           elementType: 'geometry',
-//           stylers: [{color: '#263c3f'}]
-//         },
-//         {
-//           featureType: 'poi.park',
-//           elementType: 'labels.text.fill',
-//           stylers: [{color: '#6b9a76'}]
-//         },
-//         {
-//           featureType: 'road',
-//           elementType: 'geometry',
-//           stylers: [{color: '#38414e'}]
-//         },
-//         {
-//           featureType: 'road',
-//           elementType: 'geometry.stroke',
-//           stylers: [{color: '#212a37'}]
-//         },
-//         {
-//           featureType: 'road',
-//           elementType: 'labels.text.fill',
-//           stylers: [{color: '#9ca5b3'}]
-//         },
-//         {
-//           featureType: 'road.highway',
-//           elementType: 'geometry',
-//           stylers: [{color: '#746855'}]
-//         },
-//         {
-//           featureType: 'road.highway',
-//           elementType: 'geometry.stroke',
-//           stylers: [{color: '#1f2835'}]
-//         },
-//         {
-//           featureType: 'road.highway',
-//           elementType: 'labels.text.fill',
-//           stylers: [{color: '#f3d19c'}]
-//         },
-//         {
-//           featureType: 'transit',
-//           elementType: 'geometry',
-//           stylers: [{color: '#2f3948'}]
-//         },
-//         {
-//           featureType: 'transit.station',
-//           elementType: 'labels.text.fill',
-//           stylers: [{color: '#d59563'}]
-//         },
-//         {
-//           featureType: 'water',
-//           elementType: 'geometry',
-//           stylers: [{color: '#17263c'}]
-//         },
-//         {
-//           featureType: 'water',
-//           elementType: 'labels.text.fill',
-//           stylers: [{color: '#515c6d'}]
-//         },
-//         {
-//           featureType: 'water',
-//           elementType: 'labels.text.stroke',
-//           stylers: [{color: '#17263c'}]
-//         }
-//       ]
-//     });
-// }
